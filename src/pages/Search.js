d95b54030cacada88cf137657a78f81bd7277032
Hisham Kader <hisham319@hotmail.com>
2021-01-25 00:19:55 -0500

    debbuging

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import API from "../utils/API"
 import SearchBar from "../components/searchbar"
 import SearchResults from "../components/searchresults"
