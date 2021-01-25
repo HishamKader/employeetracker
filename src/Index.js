927eacb2e15b8ea1963b98cd0118d511b03c7459
Hisham Kader <hisham319@hotmail.com>
2021-01-25 00:18:15 -0500

    new css files and fixed bugs

--- src/Index.js
+++ src/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 
 ReactDOM.render(
@@ -11,5 +11,5 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+
 
